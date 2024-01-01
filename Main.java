8ab2b9a943f537e106f52d96995a23a562cec222
Mavrick <119401508+Mavrickkkk@users.noreply.github.com>
2024-01-01 18:59:07 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,15 @@
+// Main.java
+import javax.swing.*;
 import java.io.*;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        // Exemple d'utilisation
-        Lotterie lottery = new Lotterie();
-
-        // Vendre un billet
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
-        String serialNumber = "ABC123XYZ456";
-        Ticket ticket = new Ticket(numbers, serialNumber);
-        lottery.sellTicket(ticket);
-
-        // Charger les billets depuis le disque
-        lottery.loadTicketsFromDisk();
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new LotterieGUI();
+            }
+        });
     }
 }
